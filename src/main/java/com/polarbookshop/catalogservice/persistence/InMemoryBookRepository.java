38ac6468c08110516cef3ed04208323e55f42a28
luminecs <wangxuke.china@gmail.com>
2025-10-30 17:00:35 +0800

    spring data jdbc

--- src/main/java/com/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
@@ -1,7 +1,6 @@
 package com.polarbookshop.catalogservice.persistence;
 
 import com.polarbookshop.catalogservice.domain.Book;
-import com.polarbookshop.catalogservice.domain.BookRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
@@ -9,33 +8,33 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
-public class InMemoryBookRepository implements BookRepository {
+public class InMemoryBookRepository /*implements BookRepository*/ {
     private static final Map<String, Book> books = new ConcurrentHashMap<>();
 
-    @Override
+    //    @Override
     public Iterable<Book> findAll() {
         return books.values();
     }
 
-    @Override
+    //    @Override
     public Optional<Book> findByIsbn(String isbn) {
         return existsByIsbn(isbn)
                 ? Optional.of(books.get(isbn))
                 : Optional.empty();
     }
 
-    @Override
+    //    @Override
     public boolean existsByIsbn(String isbn) {
         return books.get(isbn) != null;
     }
 
-    @Override
+    //    @Override
     public Book save(Book book) {
         books.put(book.isbn(), book);
         return book;
     }
 
-    @Override
+    //    @Override
     public void deleteByIsbn(String isbn) {
         books.remove(isbn);
     }
